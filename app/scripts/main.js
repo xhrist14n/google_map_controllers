d5a9b7dfcf35d7bdb4956ed5b111124ea8388561
khrist14n <khrist14n@gmail.com>
2016-10-27 01:04:38 -0500

    google maps sample

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,27 +22,15 @@ function initialize() {
   };
   var map = new google.maps.Map(mapDiv, mapOptions);
 
-  var controlText = [
-      //['TOP_LEFT', google.maps.ControlPosition.TOP_LEFT],
-      ['LEFT_TOP', google.maps.ControlPosition.LEFT_TOP],
-      //['TOP_CENTER', google.maps.ControlPosition.TOP_CENTER],
-      //['TOP_RIGHT', google.maps.ControlPosition.TOP_RIGHT],
-      //['RIGHT_TOP', google.maps.ControlPosition.RIGHT_TOP],
-      ['LEFT_CENTER', google.maps.ControlPosition.LEFT_CENTER],
-      //['RIGHT_CENTER', google.maps.ControlPosition.RIGHT_CENTER],
-      //['BOTTOM_LEFT', google.maps.ControlPosition.BOTTOM_LEFT],
-      ['LEFT_BOTTOM', google.maps.ControlPosition.LEFT_BOTTOM],
-      //['BOTTOM_CENTER', google.maps.ControlPosition.BOTTOM_CENTER],
-      //['BOTTOM_RIGHT', google.maps.ControlPosition.BOTTOM_RIGHT],
-      //['RIGHT_BOTTOM', google.maps.ControlPosition.RIGHT_BOTTOM]
-    ];
 
-  for (var i = 0; i < controlText.length; i++) {
-    var divLabel = controlText[i][0];
+    var divLabel = 'LEFT_CENTER';
     var divName = document.createElement('div');
+    divName.id = 'center-control';
     var newDiv = new MakeControl(divName, divLabel);
-    map.controls[controlText[i][1]].push(divName);
-  }
+    map.controls[google.maps.ControlPosition.LEFT_CENTER].push(divName);
+
+    $("#center-control").addClass("text-orientation-vertical");
+
 }
 
 $(
