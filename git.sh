2f39ecf73a17db009c9e081705115967cd9d8a6d
khrist14n <khrist14n@gmail.com>
2016-10-27 00:31:42 -0500

    google maps sample

---
+++ git.sh
@@ -0,0 +1,13 @@
+git add app -f
+git add .bowerrc
+git add .editorconfig
+git add .gitattributes
+git add .gitignore
+git add .ro.rc.json
+git add bower.json
+git add git.sh
+git add *.js
+git add *.json
+git add *.md
+git commit -am "google maps sample"
+git push origin master
